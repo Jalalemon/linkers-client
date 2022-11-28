eb1209c2ae2a024dc1b07eaaed395974357df302
Your Name <ju679935@gmail.com>
2022-11-28 09:19:20 +0600

    banner added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,20 +3,22 @@ import bannerImg from '../../assets/banner.png'
 const Banner = () => {
     return (
       <div>
-        <div className="hero">
+        <div className="hero mt-20">
           <div className="hero-content flex-col lg:flex-row-reverse">
             <img
-              src={bannerImg} alt=''
+              src={bannerImg}
+              alt=""
               className="rounded-lg w-1/2 shadow-2xl"
             />
             <div>
-              <h1 className="text-5xl font-bold">Box Office News!</h1>
+              <h1 className="text-5xl font-bold">Linkers.com</h1>
               <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
+                Each day, we are fueling commerce and accelerating
+                digitalisation across Bangladesh.Four of the most exciting
+                emerging economies in the world, and we are committed to
+                unlocking their potential.
               </p>
-              <button className="btn btn-primary">Get Started</button>
+              <button className="btn btn-secondary">Get Started</button>
             </div>
           </div>
         </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,11 +3,14 @@ import Advertise from './Advertise';
 import Banner from './Banner';
 import Categories from './Categories';
 import ThereCategory from './ThereCategory';
-
+import img from '../../assets/frezbanner.jpg'
 const Home = () => {
     return (
-      <div className="mx-6">
+      <div className="mx-6 grid">
         <Banner></Banner>
+        <div>
+            <img src={img} alt="" className='mt-20' />
+        </div>
         <div className="text-3xl my-12 font-bold"></div>
         <Advertise></Advertise>
         <div className="text-3xl my-12 font-bold"> All Category</div>
